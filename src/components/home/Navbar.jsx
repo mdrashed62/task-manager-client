bd0e6c52013b5423154e54bf9e1a346648b26387
mdrashed62 <mdrashedbiswas83@gmail.com>
2025-11-25 08:46:30 +0600

    bug solved

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -25,8 +25,6 @@ function Navbar() {
     }
   };
 
-
-
   return (
     <nav className="bg-slate-800 text-white/80 shadow-lg py-4 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -82,7 +80,12 @@ function Navbar() {
                 )}
               </div>
             ) : (
-              "X"
+              <button
+                onClick={() => navigate("/login")}
+                className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition font-semibold"
+              >
+                Login
+              </button>
             )}
           </div>
         </div>
