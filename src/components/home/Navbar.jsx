c086ec3383781af2965a90ea445b19f17613acd8
mdrashed62 <mdrashedbiswas83@gmail.com>
2025-11-25 09:07:21 +0600

    remove login button

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -25,6 +25,8 @@ function Navbar() {
     }
   };
 
+
+
   return (
     <nav className="bg-slate-800 text-white/80 shadow-lg py-4 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -80,12 +82,7 @@ function Navbar() {
                 )}
               </div>
             ) : (
-              <button
-                onClick={() => navigate("/login")}
-                className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition font-semibold"
-              >
-                Login
-              </button>
+              "X"
             )}
           </div>
         </div>
